198d124dec692a1b47790971675ebe456c466806
ZainAbdullah046 <zainabdullah222046@gmail.com>
2024-05-29 17:29:29 +0500

    local storage

--- lib/Login.dart
+++ lib/Login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -23,9 +24,39 @@ class _MyWidgetState extends State<Login> {
         ),
         backgroundColor: Colors.blue,
       ),
-      body: const Center(
+      body: Center(
         child: Column(
-          children: [],
+          children: [
+            Container(
+              // Container 2
+              width: 200, // Adjust width as needed
+              height: 200, // Adjust height as needed
+              color: Colors.green, // Example color
+              child: Center(
+                child: Image.asset(
+                  'assets/images/loginicon.jpg', // Path to your image asset
+                  width: 100, // Adjust width as needed
+                  height: 100, // Adjust height as needed
+                ),
+              ),
+            ),
+            Container(
+              width: 200,
+              height: 200,
+              color: Colors.blue,
+              child: const Center(
+                child: Text("Container 1"),
+              ),
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.blue,
+              child: const Center(
+                child: Text("Container 2"),
+              ),
+            ),
+          ],
         ),
       ),
     );
